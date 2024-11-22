e2454a598938c7ef1cf47f6af0406fac00268002
Ilya Bagrov <ilya.b.only@gmail.com>
2024-11-22 22:15:32 +0300

    Add "wraps" decorator and remove blank lines

--- task1/solution.py
+++ task1/solution.py
@@ -1,25 +1,23 @@
+from functools import wraps
 from typing import Any, Callable
 
 
 def strict(func: Callable) -> Callable:
+    @wraps(func)
     def inner(*args, **kwargs) -> Any:
         annots = func.__annotations__
-
         for arg, annot_name in zip(args, annots):
             if type(arg) != annots[annot_name]:
                 raise TypeError(
                     f"Parameter '{annot_name}' has wrong type {type(arg)}, "
                     f"expected {annots[annot_name]}"
                 )
-
-
         for kwarg_name in kwargs:
             if type(kwargs[kwarg_name]) != annots[kwarg_name]:
                 raise TypeError(
                     f"Parameter '{kwarg_name}' has wrong type "
                     f"{type(kwargs[kwarg_name])}, expected {annots[kwarg_name]}"
                 )
-
         result = func(*args, **kwargs)
         return result
     return inner

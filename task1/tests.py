9f158a06a1ec025a1fc11ff65035acb3900af2ce
Ilya Bagrov <ilya.b.only@gmail.com>
2024-11-23 17:04:21 +0300

    Refactor tests for task1

--- task1/tests.py
+++ task1/tests.py
@@ -1,3 +1,5 @@
+import unittest
+
 from solution import strict
 
 
@@ -10,35 +12,27 @@ def sum_two_with_kwargs(a: int, b: int, *, c: str, d: bool) -> int:
     return a + b
 
 
-def test_positive_case_args_only():
-    assert sum_two(1, 2) == 3
+class TestStrict(unittest.TestCase):
+    """Test decorator '@strict' with two functions that accept positional only
+    and positional and keyword arguments."""
 
-def test_positive_case_with_kwargs():
-    assert sum_two_with_kwargs(1, 2, c='str', d=True) == 3
+    def test_positive_case_args_only(self):
+        self.assertTrue(sum_two(1, 2) == 3)
 
-def test_raise_type_error():
-    try:
-        sum_two(1, '2')
-    except TypeError:
-        pass
-    else:
-        raise AssertionError(
-            'Test passed, but TypeError exception was expected'
-        )
+    def test_positive_case_with_kwargs(self):
+        self.assertTrue(sum_two_with_kwargs(1, 2, c='string', d=True) == 3)
 
-def test_raise_type_error_with_kwargs():
-    try:
-        sum_two_with_kwargs(1, 2, c=False, d='str')
-    except TypeError:
-        pass
-    else:
-        raise AssertionError(
-            'Test passed, but TypeError exception was expected'
-        )
+    def test_raise_type_error(self):
+        self.assertRaises(TypeError, sum_two, 1, '2')
+
+    # Different usecases of 'assertRaises*' are used only for demonstration
+    def test_raise_type_error_with_kwargs(self):
+        with self.assertRaisesRegex(
+            TypeError,
+            "Parameter 'd' has wrong type <class 'str'>, expected <class 'bool'>"
+        ):
+            sum_two_with_kwargs(1, 2, c='string', d='string too')
 
 
 if __name__ == '__main__':
-    test_positive_case_args_only()
-    test_positive_case_with_kwargs()
-    test_raise_type_error()
-    test_raise_type_error_with_kwargs()
+    unittest.main()
